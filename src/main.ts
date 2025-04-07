c0efe6e69eb3d3ad78424d3db2dd49541db63014
Sergey Sorokin <albuspersiv19@gmail.com>
2025-04-07 21:18:29 +0300

    feat: запущен сервер с методами GET, POST

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
-const a = 123;
+import express, { Request, Response } from 'express';
 
-console.log(a);
+const server = express();
+
+server.get('/', (req: Request, res: Response) => {
+  console.log('Пришёл GET / запрос');
+
+  res.json({ message: 'Hello World!' });
+});
+
+server.post('/', (req: Request, res: Response) => {
+  console.log('Пришёл POST / запрос');
+
+  res.send({ message: 'Goodbye World!' });
+});
+
+const port = 2000;
+
+server.listen(port, () => {
+  console.log(`Server is started on port ${port}`);
+});
