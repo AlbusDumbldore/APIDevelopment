16dc60a03adef7f58e94aaa5100ed4a24456cca6
Sergey Sorokin <albuspersiv19@gmail.com>
2025-04-08 08:51:30 +0300

    feat: типо попытался добавить обработчики

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,32 @@ import express from 'express';
 const server = express();
 server.use(express.json());
 
-server.get('/user/:id/news/:newsId', (req, res) => {
-  const { id, newsId } = req.params;
-  res.send('Держите новости!');
-});
+// server.get('/user/:id/news/:newsId', (req, res) => {
+//   const { id, newsId } = req.params;
+//   res.send('Держите новости!');
+//   res.json({
+//     id: newsId,
+//     newsId: req.params.id,
+//   });
+// });
 
-// не до конца понял Path параметры ?
+// server.get('', (req, res) => {
+//   console.log('Пришли параметры:');
+//   console.log(req.query);
+//   res.send('Hello!');
+// });
 
-server.get('', (req, res) => {
-  console.log('Пришли параметры:');
-  console.log(req.query);
-  res.send('Hello World!');
-});
-
-server.post('', (req, res) => {
-  console.log('Пришло тело:');
+server.post('/task/:id/task/:title/task/:description', (req, res) => {
+  console.log('Пришла задача');
   console.log(req.body);
-  res.send('Hello World!');
+  res.send(`Задача ${req.body.id}. ${req.body.title} \n Описание: ${req.body.description}`);
+});
+
+server.get('/user/profile', (req, res) => {
+  console.log('Инициализация входа');
+  console.log(req.query);
+  const user = ({ id, name, email, password, role } = req.params);
+  res.send(`Добро пожаловать, ${req.body.name}`);
 });
 
 const port = 2000;
