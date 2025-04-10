fa7a5148693e864bf94781094d3d3cea6ea58c83
Sergey Sorokin <albuspersiv19@gmail.com>
2025-04-10 16:35:10 +0300

    feat: добавлена функция logRoutes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { logRoutes } from './bootstrap';
 import { taskRouter } from './modules/task/task.router';
 import { userRouter } from './modules/user/user.router';
 
@@ -9,6 +10,8 @@ server.use('/user', userRouter);
 server.use('/task', taskRouter);
 
 const port = 2000;
+
+logRoutes(server);
 
 server.listen(port, () => {
   console.log(`Server is started on port ${port}`);
