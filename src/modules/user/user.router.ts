f89458dd8de0995e07f9a2072d05f4c26ab9adc7
Sergey Sorokin <albuspersiv19@gmail.com>
2025-04-15 08:21:56 +0300

    feat: добавлен errorHandler

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -7,13 +7,17 @@ userRouter.post('/register', (req: Request, res: Response) => {
   const body = req.body;
   logger.info('Регистрация нового пользователя');
 
-  res.json({ message: 'Вы проходите процесс регистрации' });
+  if (body.username.length < 5) {
+    throw Error('Регистрация не удалась');
+  }
+
+  res.json({ message: 'Вы проходите процесс регистрации', body });
 });
 
 userRouter.post('/login', (req: Request, res: Response) => {
-  logger.info('Процесс авторизации пользователя');
+  logger.info('Процесс аутентификации пользователя');
 
-  res.json({ message: 'Вы проходите процесс авторизации' });
+  res.json({ message: 'Вы проходите процесс аутентификации' });
 });
 
 userRouter.get('/profile', (req: Request, res: Response) => {
