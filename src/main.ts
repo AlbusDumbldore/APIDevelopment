7a3d29647148f57dfceb1f98f7c923bfb032e028
Sergey Sorokin <albuspersiv19@gmail.com>
2025-04-23 23:31:16 +0300

    feat: добавлены переменные окружения

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { config } from 'dotenv';
 import express from 'express';
+import * as process from 'node:process';
 import { logRoutes } from './bootstrap';
 import logger from './logger';
 import { errorHandler, logRequestMiddleware } from './middlewares';
 import { taskController } from './modules/task/task.controller';
 import { userController } from './modules/user/user.controller';
+
+config();
 
 const bootstrap = () => {
   const server = express(); // http://localhost:2000
@@ -15,10 +19,9 @@ const bootstrap = () => {
 
   server.use(errorHandler);
 
-  const port = 2000;
-
   logRoutes(server);
 
+  const port = Number(process.env.PORT);
   server.listen(port, () => {
     logger.info(`Listening on port ${port}`);
   });
