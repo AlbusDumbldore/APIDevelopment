408c3e062682f36e435d630c8a46d963b175972b
Sergey Sorokin <albuspersiv19@gmail.com>
2025-05-01 07:34:42 +0300

    feat: добавлены переменные окружения для подключения к базе и валидация

--- src/config/app-config.dto.ts
+++ src/config/app-config.dto.ts
@@ -1,8 +1,26 @@
 import { Type } from 'class-transformer';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class AppConfigDto {
   @IsNumber()
   @Type(() => Number)
   port: number;
+}
+
+export class ConfigSequelize {
+  @IsNumber()
+  @Type(() => Number)
+  port: number;
+
+  @IsString()
+  host: string;
+
+  @IsString()
+  database: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  password: string;
 }
